385729513d1db547ed6e03b3831eb6c2fb1a229e
fibo <casati_gianluca@yahoo.it>
2017-11-09 17:37:34 +0100

    added standa

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
-var read = require('./index.js')
+const read = require('./read-file-utf8')
 
-var file = '.jshintrc'
+const file = 'example.js'
 
 console.log(read(file))
 
-read(file, function (err, data) {
-  console.log('\n===\nhas ' + data.length + ' chars')
+read(file, (err, data) => {
+  if (err) throw err
+
+  console.log(`===\nFile ${file} has ${data.length} chars`)
 })
